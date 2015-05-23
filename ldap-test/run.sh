24ea4f42979ab934d176a995ce5ad6bd2786e4c0
Josh Bregman <josh.bregman@conjur.net>
2015-05-23 11:57:17 +0000

    changed docker to use my_ldap.conf

--- ldap-test/run.sh
+++ ldap-test/run.sh
@@ -5,4 +5,4 @@ env | grep CONJUR
 
 
 
-sudo docker run -it -v $PWD/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml -v $PWD/server.xml:/usr/local/tomcat/conf/server.xml -v $PWD/conjurize.sh:/conjurize.sh -v $PWD/ldap.conf:/etc/ldap/ldap.conf -e CONJUR_HOST=$CONJUR_HOST -e TOMCAT_HOST=$TOMCAT_HOST -e CONJUR_ACCOUNT=$CONJUR_ACCOUNT --rm -p 8080:8080 tomcatldap:1.0
+sudo docker run -it -v $PWD/tomcat-users.xml:/usr/local/tomcat/conf/tomcat-users.xml -v $PWD/server.xml:/usr/local/tomcat/conf/server.xml -v $PWD/conjurize.sh:/conjurize.sh -v $PWD/my_ldap.conf:/etc/ldap/ldap.conf -e CONJUR_HOST=$CONJUR_HOST -e TOMCAT_HOST=$TOMCAT_HOST -e CONJUR_ACCOUNT=$CONJUR_ACCOUNT --rm -p 8080:8080 tomcatldap:1.0
