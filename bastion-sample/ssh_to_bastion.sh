97bfe52b9742d108b5a0a3c8f716ab24abb9a40b
Josh Bregman <joshbregman@Joshs-MacBook-Air.local>
2015-12-26 23:45:22 -0500

    Updated ssh_to_bastion.sh with --me which allows the same command for both ssh as the ubuntu user as well as the Conjur user

--- bastion-sample/ssh_to_bastion.sh
+++ bastion-sample/ssh_to_bastion.sh
@@ -4,6 +4,12 @@ instance_id=`aws cloudformation describe-stack-resources --stack-name $1 | jsonf
 ip_address=`aws ec2 describe-instances --instance-id $instance_id | jsonfield Reservations.0.Instances.0.PublicIpAddress`
  
 eval $(ssh-agent)
-ssh-add $PUBLIC_SSH_KEY
+if [ "$2" = "--me" ]; then
+     username=`conjur authn whoami | jsonfield username`
+     ssh-add $MY_SSH_KEY
+     ssh $username@$ip_address
+else
+     ssh-add $PUBLIC_SSH_KEY
+     ssh ubuntu@$ip_address 
+fi
 
-ssh ubuntu@$ip_address 
