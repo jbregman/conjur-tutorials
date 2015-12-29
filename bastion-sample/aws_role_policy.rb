ca31abe588c2c6ad8e54b7b39963e678f0b18074
Josh Bregman <joshbregman@Joshs-MacBook-Air.local>
2015-12-29 07:57:00 -0500

    Policy now has readers and updaters

--- bastion-sample/aws_role_policy.rb
+++ bastion-sample/aws_role_policy.rb
@@ -5,12 +5,33 @@ policy "aws/role/#{ENV['AWS_ROLE']}" do
      policy_resource.annotations['arn'] = "#{ENV['ROLE_ARN']}"
      policy_resource.annotations['aws policy'] = "#{ENV['AWS_POLICY']}"
 
+
+
      access_key_id = variable "AccessKeyId"
      access_key_id.resource.annotations['description'] = "AWS Access Key for this role"
+
      secret_access_key = variable "SecretAccessKey"
      secret_access_key.resource.annotations['description'] = "Secret Access Key for this role"
      session_token = variable "SessionToken"
      session_token.resource.annotations['description'] = "Session Token for this role"
 
+
+     group "readers" do |group|
+	can 'read',access_key_id
+        can 'execute',access_key_id
+	can 'read',secret_access_key
+        can 'execute',secret_access_key
+	can 'read',session_token
+        can 'execute',session_token
+     end
+
+     group "updaters" do |group|
+	can 'read',access_key_id
+        can 'update',access_key_id
+	can 'read',secret_access_key
+        can 'update',secret_access_key
+	can 'read',session_token
+        can 'update',session_token
+     end
 
 end
