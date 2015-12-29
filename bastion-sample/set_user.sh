0bb69968f996be981c4069a2409ac4b5bec20af2
Josh Bregman <joshbregman@Joshs-MacBook-Air.local>
2015-12-29 11:11:56 -0500

    Added the --no-login flag to handle the case of using a profile that is assigned to the group, not the user

--- bastion-sample/set_user.sh
+++ bastion-sample/set_user.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-conjur authn login $1 
+if [ "$2" != "--no-login" ]; then
+	conjur authn login $1 
+fi
 ln -Fs .$1.conjurenv .conjurenv
 echo Conjur user profile set to $1
-
+conjur env check
