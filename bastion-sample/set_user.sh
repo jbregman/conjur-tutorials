9cb5b701111e92ac4eb2a211c44a70171645b6aa
Josh Bregman <joshbregman@Joshs-MacBook-Air.local>
2015-12-30 17:08:11 -0500

    Rolled back permissions change

--- bastion-sample/set_user.sh
+++ bastion-sample/set_user.sh
@@ -2,7 +2,16 @@
 
 if [ "$2" != "--no-login" ]; then
 	conjur authn login $1 
+        ln -Fs .$1.conjurenv .conjurenv
+	echo Conjur user profile set to $1
+else 
+	new_env_file="$(mktemp /tmp/conjur.XXXXX)"
+        username=`conjur authn whoami | jsonfield username`		
+	
+	cat .$username.conjurenv > $new_env_file
+	cat .$1.conjurenv >> $new_env_file
+	ln -Fs $new_env_file .conjurenv
+	echo Conjur user profile set to $username with aws_role $1
+	
 fi
-ln -Fs .$1.conjurenv .conjurenv
-echo Conjur user profile set to $1
 conjur env check
